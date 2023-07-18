27646e6ea292e5c456cc2d365999a35873929994
Hamudy04 <mohammadabdallah0104@gmail.com>
2023-07-18 14:26:37 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
-#include <stdio.h>  // Include the necessary header file
+#include <stdio.h>  /* Include the necessary header file */
 
 int _putchar(char c) {
-    // Your implementation of _putchar goes here
-    // For example, if you are using it to print characters to the console:
+    /* Your implementation of _putchar goes here
+       For example, if you are using it to print characters to the console: */
     return putchar(c);
 }
 
@@ -12,7 +12,7 @@ int main() {
     _putchar('l');
     _putchar('l');
     _putchar('o');
-    _putchar('\n');  // Printing a newline character
+    _putchar('\n');  /* Printing a newline character */
 
     return 0;
 }
