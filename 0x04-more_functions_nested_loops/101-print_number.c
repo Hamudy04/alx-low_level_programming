8726de1aa92ee57c32736197bb011961954e063e
Hamudy04 <mohammadabdallah0104@gmail.com>
2023-07-20 07:13:41 -0700

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,72 +1,38 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer
- * @n: The number to be printed
- *
- * Return: void
+ * print_integer - A function to print an integer.
+ * @n: An input integer
+ * Return: Nothing
  */
-void print_number(int n)
+void print_integer(int n)
 {
-	int digit, divisor;
-	int base = 9;
-	int start = 0;
-	int isNegative = 0;
-	int isMinInt = 0;
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
 	if (n < 0)
 	{
-		isNegative = 1;
-		n = n * -1;
-		isMinInt = n < 0 ? 1 : 0;
-		n = n < 0 ? (n + 1) * -1 : n;
+		_putchar('-');
+		n = -n; // Make n positive for further processing
 	}
-	while (base >= 0)
+
+	int divisor = 1000000000;
+	int leading_zero = 1; // To skip leading zeros
+
+	while (divisor >= 1)
 	{
-		divisor = to_power(10, base);
-		digit = ((n / divisor) % 10);
-		if (start == 0 && digit == 0)
+		int digit = n / divisor;
+		n %= divisor;
+		divisor /= 10;
+
+		if (digit != 0 || !leading_zero)
 		{
-			base--;
-		}
-		else if (start == 0 && digit != 0)
-		{
-			base--;
-			start = 1;
-			if (isNegative == 1)
-				_putchar('-');
 			_putchar(digit + '0');
-		}
-		else
-		{
-			if (isMinInt && divisor == 1)
-				_putchar((digit + 1) + '0');
-			else
-				_putchar(digit + '0');
-			base--;
+			leading_zero = 0;
 		}
 	}
-	if (divisor == 1 && start == 0)
-		_putchar(48);
 }
 
-/**
- * to_power - Computes the exponential of a number
- * @n: The number
- * @base: The power that n is raised to
- *
- * Return: The computed power
- */
-
-int to_power(int n, int base)
-{
-	int i = 0;
-	int exp = 1;
-
-	while (i < base)
-	{
-		exp *= n;
-		i++;
-	}
-	return (exp);
-}
