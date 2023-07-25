4bb648c0bbdf5a3a6cd19f63259669411ea43d70
Hamudy04 <mohammadabdallah0104@gmail.com>
2023-07-25 14:47:11 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strlen - check the code
+ * @s: The str whose length is to be calculated
+ * Return: len of str
  */
-int main(void)
+int _strlen(char *s)
 {
-    char *str;
-    int len;
-
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
+	return (strlen(s));
 }
