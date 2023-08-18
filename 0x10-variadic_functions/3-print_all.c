86708d69e9242c062d2803225444531ed8d14f9b
Hamudy04 <mohammadabdallah0104@gmail.com>
2023-08-18 14:40:33 -0700

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,52 +1,26 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- * print_all - print char, integer, float and string
+ * sum_them_all - Returns sum of the arguements
+ * @n:The 1st arguement
+ * @...:The intergers to add
  *
- * @format: format
+ * Return:The sum
  */
-void print_all(const char * const format, ...)
-{
-	va_list list;
-	unsigned int j = 0, start = 0;
-	char *p;
 
-	va_start(list, format);
-	while (format && format[j] != '\0')
-	{
-		switch (format[j])
-		{ case 'c':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%c", va_arg(arg_list, int));
-			break;
-			case 'i':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%i", va_arg(arg_list, int));
-			break;
-			case 'f':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%f", va_arg(arg_list, double));
-			break;
-			case's':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			p = va_arg(arg_list, char*);
-			if (p)
-			{ printf("%s", p);
-				break; }
-			printf("%p", p);
-			break; }
-		j++;
-	}
-	printf("\n");
-	va_end(arg_list);
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, i = n;
+	va_list first;
+
+	if (!n)
+		return (0);
+	va_start(first, n);
+
+	while (i--)
+		sum += va_arg(first, int);
+
+	va_end(first);
+
+	return (sum);
 }
