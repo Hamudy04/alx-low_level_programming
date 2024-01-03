d047cb77d73828bf45753cd8290ac8f18ef49a19
Hamudy04 <mohammadabdallah0104@gmail.com>
2024-01-03 16:49:35 +0300

    C - Bit Manipulation

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -2,13 +2,15 @@
 
 /*
  * Writes the character c to stdout
- * 
+ *
  *
  * Return: 1. on success and -1. on error.
  */
 
 int _putchar(char c)
 
+
 {
-	return (write(1, &c, 1));
+        return (write(1, &c, 1));
 }
+
